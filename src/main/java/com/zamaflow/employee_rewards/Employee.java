ef3858d0c2836ff1e459e6fb6432d8f1b27490d0
wbadmin <>
2020-06-06 05:47:17 +0200

    {/src/main/java/com/zamaflow/employee_rewards/Employee.java}

--- src/main/java/com/zamaflow/employee_rewards/Employee.java
+++ src/main/java/com/zamaflow/employee_rewards/Employee.java
@@ -7,29 +7,64 @@ package com.zamaflow.employee_rewards;
 @javax.persistence.Entity
 public class Employee implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(generator = "EMPLOYEE_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "EMPLOYEE_ID_SEQ", name = "EMPLOYEE_ID_GENERATOR")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(generator = "EMPLOYEE_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(sequenceName = "EMPLOYEE_ID_SEQ", name = "EMPLOYEE_ID_GENERATOR")
+	private java.lang.Long id;
 
-    public Employee() {
-    }
-    
-    public Employee(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Department")
+	private java.lang.String dept;
 
+	@javax.persistence.ManyToOne(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Award")
+	private com.zamaflow.employee_rewards.Award award;
 
+	public Employee() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getDept() {
+		return this.dept;
+	}
+
+	public void setDept(java.lang.String dept) {
+		this.dept = dept;
+	}
+
+	public com.zamaflow.employee_rewards.Award getAward() {
+		return this.award;
+	}
+
+	public void setAward(com.zamaflow.employee_rewards.Award award) {
+		this.award = award;
+	}
+
+	public Employee(java.lang.Long id, java.lang.String name,
+			java.lang.String dept, com.zamaflow.employee_rewards.Award award) {
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.award = award;
+	}
 
 }
